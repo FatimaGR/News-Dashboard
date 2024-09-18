d29850241845e2320423088ff48b1c06f4fde858
FatimaGR <fawicoma@gmail.com>
2024-09-18 18:31:59 -0500

    Added pagitaion in NewsList component

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,13 +1,24 @@
 import NewsCard from "./NewsCard";
 import { NewsArticle } from "../interfaces";
 import { useAuth } from "../context/auth-context";
+import { useState } from "react";
 
 interface NewsListProps{
   newsList: NewsArticle[];
 };
 
+const pagination = (data:NewsArticle[], page:number, limit:number): NewsArticle[] => {
+  const startId = (page - 1) * limit;
+  const endId = page * limit;
+  const paginatedData = data?.slice(startId, endId);
+  return paginatedData;
+};
+
 function NewsList({ newsList }: NewsListProps) {
   const { user, userData } = useAuth();
+  const [page, setPage] = useState<number>(1);
+  const limit: number = 20;
+  const paginatedNewsList = pagination(newsList, page, limit);
 
   if (!user || !userData){
     return;
@@ -15,9 +26,27 @@ function NewsList({ newsList }: NewsListProps) {
 
   return (
     <div className="news-list-container">
-      {newsList.map((news, index) => (
-        <NewsCard key={index} article={news} userSavedNews={userData.savedNews}/>
-      ))}
+      <div className="news-list-content">
+        {paginatedNewsList.map((news, index) => (
+          news.title != "[Removed]"?
+          <NewsCard key={index} article={news} userSavedNews={userData.savedNews}/>
+          : <></>
+        ))}
+      </div>
+      <div className="pages-container">
+        <button 
+          disabled={page === 1} 
+          onClick={() => setPage(page - 1)}
+        >
+          Previus
+        </button>
+        <button 
+          disabled={paginatedNewsList.length < limit}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 };
